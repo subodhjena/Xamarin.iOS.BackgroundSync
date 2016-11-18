f92792100356833814067960b47b5a2aecf72def
Subodh Jena <jenasubodh@gmail.com>
2016-11-18 21:28:13 +0530

    Adds Upload Model

--- Xamarin.iOS.BackgroundSync/Models/UploadModel.cs
+++ Xamarin.iOS.BackgroundSync/Models/UploadModel.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Realms;
 
 namespace Xamarin.iOS.BackgroundSync
 {
-    public class UploadModel
+    public class UploadModel : RealmObject
     {
         public UploadModel()
         {
         }
+
+        [PrimaryKey]
+        public String Id { get; set; }
+        public int TaskIdentifier { get; set; }
+        public String FilePath { get; set; }
+        public double UploadPercentage { get; set; }
+        public int Status { get; set;}
+    }
+
+    public enum SyncStatus
+    {
+        Stopped = 0,
+        Started,
+        Failed
     }
 }
 
